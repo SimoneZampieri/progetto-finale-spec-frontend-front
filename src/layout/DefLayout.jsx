1fdaf318d508e5ad488e26b6cfd8270316b4b698
SimoneZampieri <simonezampieri106@gmail.com>
2025-05-12 10:50:55 +0200

    fix: fixed layout breaking bubble

--- src/layout/DefLayout.jsx
+++ src/layout/DefLayout.jsx
@@ -25,11 +25,11 @@ export const Layout = ({ children }) => {
               <li>
                 <Link
                   to="/compare"
-                  className="hover:text-blue-200 font-luckiest"
+                  className="hover:text-blue-200 font-luckiest relative"
                 >
                   Confronta
                   {comparisonList.length > 0 && (
-                    <span className="absolute -top-2 -right-2 bg-purple-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                    <span className="absolute -top-3 right-0 bg-purple-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
                       {comparisonList.length}
                     </span>
                   )}
